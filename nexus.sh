ab01986767f7758c02ffa804845e8457f12d5785
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-01-01 12:41:46 +0530

    Create nexus.sh

---
+++ nexus.sh
@@ -0,0 +1,6 @@
+curl -sSf https://cli.nexus.xyz/ -o install.sh
+chmod +x install.sh
+NONINTERACTIVE=1 ./install.sh
+nexus-cli register-user --wallet-address 0xC66c5848E54F24bB15c97975C12e280Cea220b55 
+nexus-cli register-node
+nohup nice -n 19 nexus-cli start --headless --check-memory --max-threads 65536 &
