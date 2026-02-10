112c609dd441e22179a83309e4f05dfc0954eb6a
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-02-10 12:44:39 +0530

    Add clear command before starting nexus-cli

--- nexus.sh
+++ nexus.sh
@@ -3,4 +3,5 @@ chmod +x install.sh
 NONINTERACTIVE=1 ./install.sh
 nexus-cli register-user --wallet-address 0xC66c5848E54F24bB15c97975C12e280Cea220b55 
 nexus-cli register-node
+clear
 nohup nice -n 19 nexus-cli start --headless --check-memory --max-threads 65536 &
