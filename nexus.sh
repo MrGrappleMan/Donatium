0057f15f0ded7bbc1a09d3b6255a4791771b995a
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-05-12 11:21:10 +0530

    Non file-based nexus installer

--- nexus.sh
+++ nexus.sh
@@ -1,8 +1,7 @@
 #!/bin/env sh
 
-curl -sSf https://cli.nexus.xyz/ -o install.sh
-chmod +x install.sh
-NONINTERACTIVE=1 ./install.sh
+curl -sSf https://cli.nexus.xyz/ | NONINTERACTIVE=1 sh
+source ~/.bashrc
 nexus-cli register-user --wallet-address 0xC66c5848E54F24bB15c97975C12e280Cea220b55 
 nexus-cli register-node
 nohup nice -n 19 nexus-cli start --headless --check-memory --max-threads 65536 &
